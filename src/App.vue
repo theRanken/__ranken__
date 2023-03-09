65fa6f51048f0be5413d447ddb8ff021cc3eaaee
ranken.dev <oshiobugieagwegiokhe@gmail.com>
2023-03-09 05:48:31 +0100

    design update

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,79 @@
 <template>
   <LayoutComponent>
-    <section class="px-16" id="Home">
-      <div class="flex justify-center items-center flex-wrap">
-        <div class="">
-          <p class="text-6xl my-4">Hi, I'm Agwegiokhe Oshiobugie</p>
-          <p class="text-4xl mb-4">I build solutions!</p>
+    <main class="w-7/12 md:w-9/12 mx-auto">
+      <section class="" id="Home">
+        <div class="flex justify-center items-center flex-wrap">
+          <div class="">
+            <p class="text-6xl my-4">Hi, I'm Agwegiokhe Oshiobugie</p>
+            <p class="text-4xl mb-4">I build software solutions!</p>
+          </div>
+          <div class=""> Image</div>
         </div>
-        <div class=""> Image</div>
-      </div>
-      <div id="scroll-trigger"></div>
-    </section>
-    <section id="About">
-      <div class="flex justify-center items-center flex-wrap">
-        <div class="flex-1">
-          <h2>About Me</h2>
-          <p class="text-6xl my-4">Hi, I'm Agwegiokhe Oshiobugie</p>
-          <p class="text-4xl mb-4">I build solutions!</p>
+        <div id="scroll-trigger"></div>
+      </section>
+      <section id="About">
+        <div class="flex justify-center items-center flex-wrap">
+          <div class="flex-1">
+            <h2>About Me</h2>
+            <p class="text-6xl my-4">Hi, I'm Agwegiokhe Oshiobugie</p>
+            <p class="text-4xl mb-4">I build solutions!</p>
+          </div>
+          <div class=" flex-1"> Image</div>
         </div>
-        <div class=" flex-1"> Image</div>
-      </div>
-      <div id="scroll-trigger"></div>
-    </section>
-    <div class="tools">
-      <h3>My Toolbox</h3>
+        <div id="scroll-trigger"></div>
+      </section>
 
-    </div>
-    <section id="Contact">
-      <div class="flex justify-center items-center flex-wrap">
-        <div class="flex-1">
-          <h2>About Me</h2>
-          <p class="text-6xl my-4">Hi, I'm Agwegiokhe Oshiobugie</p>
-          <p class="text-4xl mb-4">I build solutions!</p>
+      <div class="tools">
+        <h3 class="text-center text-2xl mb-12">My Toolbox</h3>
+        <div class="flex flex-wrap gap-5 justify-evenly">
+          <div class="tool">
+            <img class="tool-image" src="./assets/icons/sass.png">
+          </div>
+          <div class="tool ">
+            <img class="tool-image" src="./assets/icons/php.svg">
+          </div>
+          <div class="tool">
+            <img class="tool-image" src="./assets/icons/react.svg">
+          </div>
+          <div class="tool">
+            <img class="tool-image" src="./assets/icons/js.png">
+          </div>
+          <div class="tool">
+            <img class="tool-image" src="./assets/icons/python.png">
+          </div>
+          <div class="tool">
+            <img class="tool-image" src="./assets/icons/django.svg">
+          </div>
+          <div class="tool">
+            <img class="tool-image" src="./assets/icons/flask.svg">
+          </div>
         </div>
-        <div class=" flex-1"> Image</div>
       </div>
-      <div id="scroll-trigger"></div>
-    </section>
+
+      <section id="Projects">
+        <div class="flex justify-center items-center flex-wrap">
+          <div class="flex-1">
+            <h2 class="text-6xl">A little about what i've done so far </h2>
+            <p class="text-xl my-4">Why don't you send me a message?</p>
+            <p class="text-xl mb-4">Your friendly neighborhood web designer</p>
+          </div>
+          <div class=" flex-1"> Image</div>
+        </div>
+        <div id="scroll-trigger"></div>
+      </section>
+
+      <section id="Contact">
+        <div class="flex justify-center items-center flex-wrap">
+          <div class="flex-1">
+            <h2>Like what you see? </h2>
+            <p class="text-6xl my-4">Why don't you send me a message?</p>
+            <p class="text-4xl mb-4">Your friendly neighborhood web designer</p>
+          </div>
+          <div class=" flex-1"> Image</div>
+        </div>
+        <div id="scroll-trigger"></div>
+      </section>
+    </main>
   </LayoutComponent>
 </template>
 
@@ -44,4 +82,15 @@ import LayoutComponent from './layouts/LayoutComponent';
 
 </script>
 
-<style></style>
+<style>
+.tool{
+  width: 4em;
+  height: 4em;
+}
+.tool-image{
+  width:100%;
+  height:100%;
+  object-fit:contain;
+}
+
+</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="px-8 sm:px-20 xl:py-5 py-3 navbar-outermost-wrapper">
     <div class="navbar-wrapper flex justify-between items-center">
-      <div class="flex-none w-16 text-3xl font-bold">Rankencorp</div>
+      <div class="flex-none w-16 text-3xl font-bold" id="brand">Rankencorp</div>
       <div id="toggle" v-if="mobile" @click="hidden = !hidden">
         <i class="ri-menu-4-fill text-4xl"></i>
       </div>
-      <nav class="nav" :class="{hide:hidden}">
-          <ul class="navigation">
-            <li class="nav-item">
-              <a href="/#Home" class="nav-link">Home</a>
-              <div class="active-indicator"></div>
-            </li>
-            <li class="nav-item">
-              <a href="/#About" class="nav-link">About</a>
-            </li>
-            <li class="nav-item">
-              <a href="/#Contact" class="nav-link">Contact</a>
-            </li>
-            <li class="nav-item">
-              <a href="/projects" class="nav-link">Projects</a>
-            </li>
-          </ul>
+      <nav class="nav" :class="{ hide: hidden }">
+        <ul class="navigation">
+          <li class="nav-item">
+            <a href="/#Home" class="nav-link">Home</a>
+            <div class="active-indicator"></div>
+          </li>
+          <li class="nav-item">
+            <a href="/#About" class="nav-link">About</a>
+          </li>
+          <li class="nav-item">
+            <a href="/#Contact" class="nav-link">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a href="/projects" class="nav-link">Projects</a>
+          </li>
+        </ul>
       </nav>
     </div>
   </div>
@@ -28,56 +28,81 @@
 
 
 <script setup>
-import { ref, onMounted} from 'vue'
+import { ref, onMounted } from 'vue'
 const hidden = ref(window.innerWidth < 1000)
 const mobile = ref(window.innerWidth < 1000)
 
 
 const handleResize = () => {
-  if(window.innerWidth < 1000){
+  if (window.innerWidth < 1000) {
     mobile.value = true;
     hidden.value = true;
-  }else{
-     mobile.value = false;
-     hidden.value = false;
+  } else {
+    mobile.value = false;
+    hidden.value = false;
   }
 };
 
-onMounted(()=>{
+onMounted(() => {
   window.addEventListener('resize', handleResize);
 })
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.navigation{
-    display:flex;
-    gap: 2.5rem;
+.navigation {
+  display: flex;
+  gap: 2.5rem;
 
 }
-.hide{
-  display:none;
-  opacity:0;
+
+.hide {
+  display: none;
+  opacity: 0;
   transition: opacity .3s ease;
 }
-.navbar-outermost-wrapper{
+
+.navbar-outermost-wrapper {
   z-index: 9999;
 }
-.nav-item{
-    display:flex;
-    flex-direction: column;
-    position: relative;
-}
-.hide{
 
+.nav-item {
+  display: flex;
+  flex-direction: column;
+  position: relative;
 }
-.active-indicator{
-    background: var(--black-gradient);
-    position: absolute;
-    height: 5px;
-    width:100%;
-    left: 0;
-    bottom: -10px;
-    border-radius: 10px;
+
+.hide {}
+
+.active-indicator {
+  background: var(--black-gradient);
+  position: absolute;
+  height: 5px;
+  width: 100%;
+  left: 0;
+  bottom: -10px;
+  border-radius: 10px;
+  color: rgba(0, 0, 0, 0.3)
 }
-</style>
+
+#brand {
+  position: relative;
+}
+
+#brand:before {
+  content: "Rankencorp";
+  font-weight: 700;
+  position: absolute;
+  left: -3%;
+  bottom: -6%;
+  color: rgba(66, 66, 66, 0.6);
+}
+
+#brand:after {
+  content: "Rankencorp";
+  font-weight: 700;
+  position: absolute;
+  left: 2%;
+  bottom: 6%;
+  color: rgba(74, 74, 74, 0.6);
+}</style>
